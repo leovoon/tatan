698f91a8bff98e0192e9891eca85b6226e71d57b
Leo Voon <voonlihhaur@gmail.com>
2023-08-24 22:22:23 +0800

    revert: page transition

--- src/routes/[query]/+layout.svelte
+++ src/routes/[query]/+layout.svelte
@@ -2,9 +2,6 @@
 	import { onMount, type ComponentType } from 'svelte';
 	import { page } from '$app/stores';
 	import { i } from '@inlang/sdk-js';
-	import { fly } from 'svelte/transition';
-
-	export let data;
 
 	let pagination: ComponentType;
 
@@ -18,11 +15,7 @@
 <header>
 	<h1>🔍 {i('search-title')}</h1>
 </header>
-{#key data.url}
-	<div in:fly={{ x: 50, duration: 300, delay: 300 }} out:fly={{ x: -50, duration: 300 }}>
-		<slot />
-	</div>
-{/key}
+<slot />
 
 <svelte:component this={pagination} {totalResults} />
 
